b4d9deed9e87b9c8b3b85d1614e28494b5ee06bb
Whitbyfish <Matj1@aol.com>
2018-01-28 17:59:08 +0000

    added examples

---
+++ php1.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>PHP Test</title>
+</head>
+<body>
+<?php
+echo
+'<p>Hello World</p>';
+?>
+<xmp>
+  <html>
+  <head>
+  <title>PHP Test</title>
+  </head>
+  <body>
+  <?php
+  echo
+  '<p>Hello World</p>';
+  ?>
+</body>
+</html>
+</xmp>
+</body>
+</html>
