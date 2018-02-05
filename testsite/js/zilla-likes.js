cabfffc83ee8088a1ccc8288f801fbe5b9315bf5
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-02-05 13:30:36 +0000

    h

---
+++ testsite/js/zilla-likes.js
@@ -0,0 +1,40 @@
+jQuery(function($) {
+
+  $('#timeline').on('click', '.zilla-likes', function() {
+    var link = $(this);
+    if (link.hasClass('active')) return false;
+
+    link.addClass('active');
+
+    var id = $(this).attr('id'),
+        postfix = link.find('.zilla-likes-postfix').text();
+
+    $.ajax({
+      type: 'POST',
+      url: zilla_likes.ajaxurl,
+      data: {
+        action: 'zilla-likes',
+        likes_id: id,
+        postfix: postfix,
+      },
+      xhrFields: {
+        withCredentials: true,
+      },
+      success: function(data) {
+        link.html(data).addClass('active').attr('title','You already like this');
+      },
+    });
+
+    return false;
+  });
+
+  if ($('body.ajax-zilla-likes').length) {
+    $('.zilla-likes').each(function() {
+      var id = $(this).attr('id');
+      $(this).load(zilla_likes.ajaxurl, {
+        action: 'zilla-likes',
+        post_id: id,
+      });
+    });
+  }
+});
