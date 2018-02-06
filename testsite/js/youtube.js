a09cb120eaaac33f433bb45974e554eee1e14948
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-02-06 05:08:50 +0000

    d

--- testsite/js/youtube.js
+++ testsite/js/youtube.js
@@ -1,4 +1,5 @@
 function GetVidData(vidUrl){
+  alert("Got vid url " + vidUrl);
   var url = ("https://www.googleapis.com/youtube/v3/videospart=statistics&id=" + vidUrl + "&key=AIzaSyD6XBI5r8UWTPCtF00EwJOb5ZlxunvxYTw");
 var likes;
 $.getJSON(url,
