2121c402bc127503873bb8181db02e0cc2eca2a9
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-02-06 05:04:45 +0000

    d

--- testsite/js/youtube.js
+++ testsite/js/youtube.js
@@ -1,7 +1,9 @@
-var url = "https://www.googleapis.com/youtube/v3/videospart=statistics&id=0RLMAoanOVE&key=AIzaSyD6XBI5r8UWTPCtF00EwJOb5ZlxunvxYTw";
+function GetVidData(vidUrl){
+  var url = ("https://www.googleapis.com/youtube/v3/videospart=statistics&id=" + vidUrl + "&key=AIzaSyD6XBI5r8UWTPCtF00EwJOb5ZlxunvxYTw");
 var likes;
 $.getJSON(url,
     function(response){
         likes = response.data.items[0].statistics.likeCount;
         alert(likes);
 });
+}
