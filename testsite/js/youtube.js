d127a915b9fbed25d07d0b0ce23db9448339f267
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-02-09 16:15:24 +0000

    d

--- testsite/js/youtube.js
+++ testsite/js/youtube.js
@@ -35,13 +35,8 @@ $.getJSON(url,
 						width: data.items[0].snippet.thumbnails.medium.width,
 						height: data.items[0].snippet.thumbnails.medium.height
 					}).appendTo("#video-data-1");
-					$("<h1></h1>").text(data.items[0].snippet.title).appendTo("#video-data-1");
-					$("<p></p>").text(data.items[0].snippet.description).appendTo("#video-data-1");
-					$("<li></li>").text("Published at: " + data.items[0].snippet.publishedAt).appendTo("#video-data-2");
 					$("<li></li>").text("View count: " + data.items[0].statistics.viewCount).appendTo("#video-data-2");
-					$("<li></li>").text("Favorite count: " + data.items[0].statistics.favoriteCount).appendTo("#video-data-2");
 					$("<li></li>").text("Like count: " + data.items[0].statistics.likeCount).appendTo("#video-data-2");
-					$("<li></li>").text("Dislike count: " + data.items[0].statistics.dislikeCount).appendTo("#video-data-2");
 				}).fail(function(jqXHR, textStatus, errorThrown) {
 					$("<p style='color: #F00;'></p>").text(jqXHR.responseText || errorThrown).appendTo("#video-data-1");
 				});
