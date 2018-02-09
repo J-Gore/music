a0944262352fc8a54600995abb6360f2c160dc98
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-02-09 16:44:37 +0000

    d

--- testsite/js/youtube.js
+++ testsite/js/youtube.js
@@ -1,3 +1,36 @@
+
+	GetVidData(vidUrl) = function(){
+		return new Promise(function(){
+			$("#video-data-1, #video-data-2").empty();
+			var videoid = vidUrl;
+			var dfrd2 = $.Deferred();
+			var matches = videoid.match(/^http:\/\/www\.youtube\.com\/.*[?&]v=([^&]+)/i) || videoid.match(/^http:\/\/youtu\.be\/([^?]+)/i);
+			if (matches) {
+				videoid = matches[1];
+			}
+			if (videoid.match(/^[a-z0-9_-]{11}$/i) === null) {
+				$("<p style='color: #F00;'>Unable to parse Video ID/URL.</p>").appendTo("#video-data-1");
+				return;
+			}
+			$.getJSON("https://www.googleapis.com/youtube/v3/videos", {
+				key: "AIzaSyD6XBI5r8UWTPCtF00EwJOb5ZlxunvxYTw",
+				part: "snippet,statistics",
+				id: videoid
+			}, function(data) {
+				if (data.items.length === 0) {
+					$("<p style='color: #F00;'>Video not found.</p>").appendTo("#video-data-1");
+					return;
+				}
+
+				var r = data.items[0].statistics.likeCount;
+				$("<li></li>").text("View count: " + data.items[0].statistics.viewCount).appendTo("#video-data-2");
+				$("<li></li>").text("Like count: " + r).appendTo("#video-data-2");
+			}).fail(function(jqXHR, textStatus, errorThrown) {
+				$("<p style='color: #F00;'></p>").text(jqXHR.responseText || errorThrown).appendTo("#video-data-1");
+			});
+		})
+	}
+
 function GetVidData(vidUrl){
 
   /*var url = ("https://www.googleapis.com/youtube/v3/videospart=statistics&id=" + vidUrl + "&key=AIzaSyD6XBI5r8UWTPCtF00EwJOb5ZlxunvxYTw&callback=false&part=statistics");
@@ -11,7 +44,7 @@ $.getJSON(url,
         alert(likes);
         alert("made it passed code " + likes + "..." + response);
 });*/
-				$("#video-data-1, #video-data-2").empty();
+				/*$("#video-data-1, #video-data-2").empty();
 				var videoid = vidUrl;
 				var dfrd2 = $.Deferred();
 				var matches = videoid.match(/^http:\/\/www\.youtube\.com\/.*[?&]v=([^&]+)/i) || videoid.match(/^http:\/\/youtu\.be\/([^?]+)/i);
@@ -32,11 +65,11 @@ $.getJSON(url,
 						return;
 					}
 
-					var r = Promise(data.items[0].statistics.likeCount);
+					var r = data.items[0].statistics.likeCount;
 					$("<li></li>").text("View count: " + data.items[0].statistics.viewCount).appendTo("#video-data-2");
 					$("<li></li>").text("Like count: " + r).appendTo("#video-data-2");
 				}).fail(function(jqXHR, textStatus, errorThrown) {
 					$("<p style='color: #F00;'></p>").text(jqXHR.responseText || errorThrown).appendTo("#video-data-1");
 				});
 
-}
+}*/
