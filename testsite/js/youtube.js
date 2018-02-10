f85bb4767e1f0e22fbda6fd858ffb0c9329fea08
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-02-10 16:44:33 +0000

    a

--- testsite/js/youtube.js
+++ testsite/js/youtube.js
@@ -10,45 +10,52 @@ do{
 }while(exit == false);
 				var vidUrl = "";
 				var appendElement;
-				for(w = 1,promise = Promise.resolve(); w < vidNo; w++){
-						currentVid = "vid_" + w + "_likes";
-						appendElement = document.getElementById(currentVid);
-						vidUrl = document.getElementById("vid_" + w).getAttribute("data-youtube");
-						var matches = vidUrl.match(/^http:\/\/www\.youtube\.com\/.*[?&]v=([^&]+)/i) || vidUrl.match(/^http:\/\/youtu\.be\/([^?]+)/i);
-						if (matches) vidUrl = matches[1];
-						if (vidUrl.match(/^[a-z0-9_-]{11}$/i) === null) {
-							$("<p style='color: #F00;'>Unable to parse Video ID/URL.</p>").appendTo(currentVid);
-							return;
-						}
-									promise = new Promise(function(resolve,reject){
-										$.getJSON("https://www.googleapis.com/youtube/v3/videos", {
-											key: "AIzaSyD6XBI5r8UWTPCtF00EwJOb5ZlxunvxYTw",
-											part: "statistics",
-											id: vidUrl
-										},function(data){
-											if(data != null) resolve(data);
-											else reject("JSON Retrieval Error");
-										});
+				var w = 1;
+				function loop(w) {
+					if(w < vidNo){
+								currentVid = "vid_" + w + "_likes";
+								appendElement = document.getElementById(currentVid);
+								vidUrl = document.getElementById("vid_" + w).getAttribute("data-youtube");
+								var matches = vidUrl.match(/^http:\/\/www\.youtube\.com\/.*[?&]v=([^&]+)/i) || vidUrl.match(/^http:\/\/youtu\.be\/([^?]+)/i);
+								if (matches) vidUrl = matches[1];
+								if (vidUrl.match(/^[a-z0-9_-]{11}$/i) === null) {
+									$("<p style='color: #F00;'>Unable to parse Video ID/URL.</p>").appendTo(currentVid);
+									return;
+								}
+											promise = new Promise(function(resolve,reject){
+												$.getJSON("https://www.googleapis.com/youtube/v3/videos", {
+													key: "AIzaSyD6XBI5r8UWTPCtF00EwJOb5ZlxunvxYTw",
+													part: "statistics",
+													id: vidUrl
+												},function(data){
+													if(data != null) resolve(data);
+													else reject("JSON Retrieval Error");
+												});
+											});
+											promise.then(function(resolve) {
+												alert("First then. " + resolve);
+												return resolve;
+											});
+											promise.then(function(resolve){
+											alert("Second then. " + resolve);
+
+										var r = resolve.items[0].statistics.likeCount;
+
+										if(r == null) r = 34;
+										alert("R = " + r);
+										return r;
 									});
-									promise.then(function(resolve) {
-										alert("First then. " + resolve);
-										return resolve;
+									promise.then(function(r){
+		//alert("got r: " + r);
+
+										//$("<li></li>").text("View count: " + data.items[0].statistics.viewCount).appendTo("#video-data-2");
+										appendElement.innerHTML = r;
+										//$("").text("Like count: " + r).appendTo(currentVid);
 									});
-									promise.then(function(resolve){
-									alert("Second then. " + resolve);
-
-								var r = resolve.items[0].statistics.likeCount;
-
-								if(r == null) r = 34;
-								alert("R = " + r);
-								return r;
-							});
-							promise.then(function(r){
-//alert("got r: " + r);
-
-								//$("<li></li>").text("View count: " + data.items[0].statistics.viewCount).appendTo("#video-data-2");
-								appendElement.innerHTML = r;
-								//$("").text("Like count: " + r).appendTo(currentVid);
-							});
+									w++;
+									loop(w);
+					}else break;
 				}
+
+
 }
