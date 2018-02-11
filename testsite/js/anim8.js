dcfbab78df34bb99a03235a5863c3d671828af08
Whitbyfish <Matj1@aol.com>
2018-02-11 19:13:16 +0000

    f

--- testsite/js/anim8.js
+++ testsite/js/anim8.js
@@ -1,1 +1,17 @@
+(function($) {
+    var element = $('.subscribe_button'),
+        originalY = element.offset().top;
 
+    // Space between element and top of screen (when scrolling)
+    var topMargin = 20;
+
+    $(window).on('scroll', function(event) {
+        var scrollTop = $(window).scrollTop();
+
+        element.stop(false, false).animate({
+            top: scrollTop < originalY
+                    ? 0
+                    : scrollTop - originalY + topMargin
+        }, 300);
+    });
+})(jQuery);
