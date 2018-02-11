77e6027e8ad6cf1ee87fb4bc522fe92c566f3f9f
Whitbyfish <Matj1@aol.com>
2018-02-11 22:53:04 +0000

    d

--- testsite/js/youtube.js
+++ testsite/js/youtube.js
@@ -1,27 +1,36 @@
-function GetVidData(w){
-var vidNo = 1;
-var exit = false;
-var currentVid = "";
-				var vidUrl = "";
-				var appendElement;
-						currentVid = "vid_" + w + "_likes";
-						appendElement = document.getElementById(currentVid);
-						vidUrl = document.getElementById("vid_" + w).getAttribute("data-youtube");
-						var matches = vidUrl.match(/^http:\/\/www\.youtube\.com\/.*[?&]v=([^&]+)/i) || vidUrl.match(/^http:\/\/youtu\.be\/([^?]+)/i);
-						if (matches) vidUrl = matches[1];
-						if (vidUrl.match(/^[a-z0-9_-]{11}$/i) === null) {
-							$("<p style='color: #F00;'>Unable to parse Video ID/URL.</p>").appendTo(currentVid);
-							return;
-						}
-									$.getJSON("https://www.googleapis.com/youtube/v3/videos", {
-											key: "AIzaSyD6XBI5r8UWTPCtF00EwJOb5ZlxunvxYTw",
-											part: "statistics",
-											id: vidUrl
-										},function(data){
-											var r = data.items[0].statistics.likeCount;
-											if(r == null) r = 34;
-											appendElement.innerHTML = r;
-										});
+function GetYoutubeData(w){
+	var currentVid = "";
+	var vidUrl = "";
+	var appendElement;
+	currentVid = "vid_" + w + "_likes";
+	appendElement = document.getElementById(currentVid);
+	vidUrl = document.getElementById("vid_" + w).getAttribute("data-youtube");
+	var matches = vidUrl.match(/^http:\/\/www\.youtube\.com\/.*[?&]v=([^&]+)/i) || vidUrl.match(/^http:\/\/youtu\.be\/([^?]+)/i);
+	if (matches) vidUrl = matches[1];
+	if (vidUrl.match(/^[a-z0-9_-]{11}$/i) === null) {
+		$("<p style='color: #F00;'>Unable to parse Video ID/URL.</p>").appendTo(currentVid);
+		return;
+	}
+	$.getJSON("https://www.googleapis.com/youtube/v3/videos", {
+		key: "AIzaSyD6XBI5r8UWTPCtF00EwJOb5ZlxunvxYTw",
+		part: "statistics",
+		id: vidUrl
+	},function(data){
+		var r = data.items[0].statistics.likeCount;
+		if(r == null) r = 34;
+		appendElement.innerHTML = r;
+	});
+}
+function GetSubscriberCount(){
+	var appendElement = document.getElementById("subscribe_button");
+	$.getJSON("https://www.googleapis.com/youtube/v3/channels", {
+		key: "AIzaSyD6XBI5r8UWTPCtF00EwJOb5ZlxunvxYTw",
+		part: "statistics",
+		id: "UCNGt4x8CYzKLCUGlfe-TQkg"
+	},function(data){
+		var r = data.items[0].statistics.subscriberCount;
+		appendElement.innerHTML += " " + r;
+	});
 }
 function GetNoVids(){
 	var vidNo = 1;
