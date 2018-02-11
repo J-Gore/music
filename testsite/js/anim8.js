1a0ede7d16d2cadea0686841cf4275c89cba11ce
Whitbyfish <Matj1@aol.com>
2018-02-11 20:19:40 +0000

    a

---
+++ testsite/js/anim8.js
@@ -0,0 +1,19 @@
+(function($) {
+    var element = $('.subscribe_button'),
+        originalY = element.offset().top;
+    // Space between element and top of screen (when scrolling)
+    var topMargin = (screen.height/2) - (screen.height/9);
+    $(window).on('scroll', function(event) {
+        var scrollTop = $(window).scrollTop();
+        element.stop(false, false).animate({
+            top: scrollTop < originalY
+                    ? 0
+                    : scrollTop - originalY + topMargin
+        }, {duration:1000,queue: false});});
+        $(window).on('scroll',function(event) {
+          $({deg:0}).animate({deg:360},{
+            duration: 1000,
+            queue: false,
+            step:function(now){
+              element.css({
+                transform: 'rotate(' + now + 'deg)'});}});})})(jQuery);
