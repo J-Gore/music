356ab3524690b987536e2ab6f653aa141b1b4fb5
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-02-12 23:11:16 +0000

    a

--- testsite/js/anim8.js
+++ testsite/js/anim8.js
@@ -2,7 +2,6 @@ function MoveSubscribe(){
 var screenwidth = screen.width;
 var pc = screenwidth - (screenwidth/4);
 var mobile = screenwidth + (screenwidth/2);
-alert("screen width = " + screenwidth + " PC moving to: " + pc + " Mobile: " + mobile);
 if(_ua.Mobile[0]){
   $("#subscribe_button").css("left",mobile);
   $("#subscribe_button").css("width",196);
