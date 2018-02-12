6eb8db83608c22dbc63d843b6d35c394f47dcf45
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-02-12 21:49:48 +0000

    a

--- testsite/js/anim8.js
+++ testsite/js/anim8.js
@@ -1,5 +1,5 @@
 function MoveSubscribe(){
 var screenleft = screen.width - (screen.width/10);
 alert("Moving to: " + screenleft);
-document.getElementById("subscribe_button").style.left = screenleft;
+$("#subscribe_button").css("left",screenleft);
 }
