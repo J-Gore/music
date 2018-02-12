ec59221421eb91571b868fc2eae406c55bbf539a
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-02-12 01:44:55 +0000

    a

--- testsite/js/youtube.js
+++ testsite/js/youtube.js
@@ -10,6 +10,7 @@ function GetYoutubeData(w){
 	},function(data){
 		var r=data.items[0].statistics.likeCount;
 		var p=data.items[0].snippet.thumbnails.maxres.url;
+		alert("got p: " + p);
 		if(r==null)r=34;
 		c.getElementsByClassName("zilla-likes-count")[0].innerHTML=r;
 		c.getElementsByTagName("img")[0].setAttribute("data-src",p);
