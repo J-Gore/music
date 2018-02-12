b6fcc6c0f83825baeb2302042030e53c0d47c489
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-02-12 22:29:39 +0000

    a

--- testsite/js/anim8.js
+++ testsite/js/anim8.js
@@ -1,8 +1,10 @@
 function MoveSubscribe(){
-var screenleft = screen.width - (screen.width/8);
-var screenleft1 = screen.width;
-var screenleft2 = screen.width/8;
-alert("screen width = " + screenleft1 + " screen width/8 = " + screenleft2 + " moving to: " + screenleft);
+  var screenwidth = screen.width;
+var pc = screenwidth - (screenwidth/8);
+var mobile = screenwidth - (screenwidth/20);
+var screenleft1 = screenwidth;
+var screenleft2 = screenwidth/8;
+alert("screen width = " + screenleft1 + " screen width/8 = " + screenleft2 + " PC moving to: " + pc + " Mobile: " + mobile);
 if(_ua.Mobile[0]) $("#subscribe_button").css("left",screenleft);
-else $("#subscribe_button").css("left",screenleft);
+else $("#subscribe_button").css("left",pc);
 }
