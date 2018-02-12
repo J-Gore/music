70f75d8994266a5baf4cac696958ac1da5cb8d30
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-02-12 23:01:07 +0000

    a

--- testsite/js/anim8.js
+++ testsite/js/anim8.js
@@ -3,6 +3,10 @@ var screenwidth = screen.width;
 var pc = screenwidth - (screenwidth/4);
 var mobile = screenwidth + (screenwidth/2);
 alert("screen width = " + screenwidth + " PC moving to: " + pc + " Mobile: " + mobile);
-if(_ua.Mobile[0]) $("#subscribe_button").css("left",mobile);
-else $("#subscribe_button").css("left",pc);
+if(_ua.Mobile[0]){
+  $("#subscribe_button").css("left",mobile);
+  $("#subscribe_button").css("width",50);
+  $("#subscribe_button").css("height",196);
+}
+else$("#subscribe_button").css("left",pc);
 }
