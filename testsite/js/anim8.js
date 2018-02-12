3f43b6cf2da26b19c05f448bf8002b00bfed843e
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-02-12 23:50:50 +0000

    k

--- testsite/js/anim8.js
+++ testsite/js/anim8.js
@@ -7,6 +7,5 @@ if(_ua.Mobile[0]){
   $("#subscribe_button").css("width",196);
   $("#subscribe_button").css("height",50);
   $("#subscribe_button").css("font-size",25);
-}
-else$("#subscribe_button").css("left",pc);
+}else $("#subscribe_button").css("left",pc);
 }
