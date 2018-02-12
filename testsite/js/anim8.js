ef29068f5ca16a2eafbcbe9a79f10d3dbb899f29
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-02-12 23:06:03 +0000

    a

--- testsite/js/anim8.js
+++ testsite/js/anim8.js
@@ -6,7 +6,7 @@ alert("screen width = " + screenwidth + " PC moving to: " + pc + " Mobile: " + m
 if(_ua.Mobile[0]){
   $("#subscribe_button").css("left",mobile);
   $("#subscribe_button").css("width",196);
-  $("#subscribe_button").css("height",50;
+  $("#subscribe_button").css("height",50);
   $("#subscribe_button").css("font-size",25);
 }
 else$("#subscribe_button").css("left",pc);
