b50ddf4e1cb52180589078eab06e1fa2e5d1a1d1
Whitbyfish <Matj1@aol.com>
2018-02-15 21:02:28 +0000

    u

--- testsite/js/anim8.js
+++ testsite/js/anim8.js
@@ -1,11 +1,11 @@
 function MoveSubscribe(){
-var screenwidth = screen.width;
-var pc = screenwidth - (screenwidth/4);
-var mobile = screenwidth + 170;
-if(_ua.Mobile[0]){
-  $("#subscribe_button").css("left",mobile);
-  $("#subscribe_button").css("width",196);
-  $("#subscribe_button").css("height",50);
-  $("#subscribe_button").css("font-size",25);
-}else $("#subscribe_button").css("left",pc);
+  var screenwidth = screen.width;
+  var pc = screenwidth - (screenwidth/4);
+  var mobile = screenwidth + 170;
+  if(_ua.Mobile[0]){
+    $("#subscribe_button").css("left",mobile);
+    $("#subscribe_button").css("width",196);
+    $("#subscribe_button").css("height",50);
+    $("#subscribe_button").css("font-size",25);
+  }else $("#subscribe_button").css("left",pc);
 }
