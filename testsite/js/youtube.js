590536c13a66f4f9da5bd7f99daff4615c6d6de0
Whitbyfish <Matj1@aol.com>
2018-02-15 20:28:37 +0000

    s

--- testsite/js/youtube.js
+++ testsite/js/youtube.js
@@ -36,6 +36,25 @@ function GetChannelStats(){
 		document.getElementById("total_views").innerHTML+=" Total Channel Views: "+y+"!";
 	});
 }
+function GetVidIds(){
+	var vidIds;
+	alert("Getting vids");
+		$.getJSON("https://www.googleapis.com/youtube/v3/playlistItems",{
+			key:"AIzaSyD6XBI5r8UWTPCtF00EwJOb5ZlxunvxYTw",
+			part:"statistics",
+			playlistId:"PL1BxM-1kDL2jDaZWiNUWk7_gOuM_m7MZr"
+			maxResults=50;
+		},function(data){
+			alert("Got data");
+			for(i=0;i<data.items.count;i++)
+			{
+				alert(data.item[i].snippet.title);
+
+			}
+		});
+
+	return vidIds;
+}
 function GetNoVids(){
 	var v=1;
 	var e=false;
