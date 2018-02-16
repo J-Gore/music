daf4b5dc08a366fcab30245aa2c51f8178b03c4e
Whitbyfish <Matj1@aol.com>
2018-02-16 01:37:57 +0000

    i

--- testsite/js/anim80o.js
+++ testsite/js/anim80o.js
@@ -4,10 +4,24 @@ function MoveSubscribe(){
   var mobile = screenwidth + (screenwidth/3);
   //screensizes: 360, 278 too far, 170 fits...
   if(_ua.Mobile[0]){
+    var exit = false;
+
     $("#subscribe_button").css("left",mobile);
     $("#subscribe_button").css("width",196);
     $("#subscribe_button").css("height",50);
     $("#subscribe_button").css("font-size",25);
+    if(screenwidth > (196 + $("#subscribe_button").css("left")))
+    {var count=100;
+      var newMobile;
+    do{
+      newMobile=count+mobile;
+        $("#subscribe_button").css("left",newMobile);
+        if(screenwidth > (196 + $("#subscribe_button").css("left")))count+=100;
+else exit=true;
+
+    }while (exit==false);
+  }
+
   }else $("#subscribe_button").css("left",pc);
 }
 $(window).resize(function(){
