385c34b32ec9b1c3cabdd74796c93af75f5a92a5
Whitbyfish <Matj1@aol.com>
2018-02-16 01:50:45 +0000

    h

--- testsite/js/anim80o.js
+++ testsite/js/anim80o.js
@@ -11,7 +11,7 @@ var pc = screenwidth + (screenwidth*30);
     $("#subscribe_button").css("width",196);
     $("#subscribe_button").css("height",50);
     $("#subscribe_button").css("font-size",25);
-    /*if(screenwidth > (196 + $("#subscribe_button").css("left")))
+    if(screenwidth > (196 + $("#subscribe_button").css("left")))
     {var count=100;
       var newMobile;
     do{
@@ -22,9 +22,23 @@ var pc = screenwidth + (screenwidth*30);
 else exit=true;
 
     }while (exit==false);
-  }*/
+  }
 
-  }else $("#subscribe_button").css("left",pc);
+  }else{
+$("#subscribe_button").css("left",pc);
+    if(screenwidth < (196 + $("#subscribe_button").css("left")))
+    {var count=100;
+      var newMobile;
+    do{
+      newMobile=count+mobile;
+      alert("IN LOOP");
+        $("#subscribe_button").css("left",newMobile);
+        if(screenwidth < (196 + $("#subscribe_button").css("left")))count+=100;
+    else exit=true;
+
+    }while (exit==false);
+    }
+  }
 }
 $(window).resize(function(){
   var screenwidth = screen.width;
