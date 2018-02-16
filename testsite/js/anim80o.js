ecf980d85f592e3c50b8cfa57934aef45da28b2a
Whitbyfish <Matj1@aol.com>
2018-02-16 01:42:17 +0000

    h

--- testsite/js/anim80o.js
+++ testsite/js/anim80o.js
@@ -1,7 +1,7 @@
 function MoveSubscribe(){
   var screenwidth = screen.width;
   var pc = screenwidth - (screenwidth/4);
-  var mobile = screenwidth + (screenwidth/3) + 10000;
+  var mobile = screenwidth + (screenwidth/3) + 1000;
   //screensizes: 360, 278 too far, 170 fits...
   if(_ua.Mobile[0]){
     var exit = false;
@@ -15,6 +15,7 @@ function MoveSubscribe(){
       var newMobile;
     do{
       newMobile=count+mobile;
+      alert("IN LOOP");
         $("#subscribe_button").css("left",newMobile);
         if(screenwidth > (196 + $("#subscribe_button").css("left")))count+=100;
 else exit=true;
